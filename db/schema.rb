284ecc8db08c07cd8d4ecfc100432df0ba5e91a0
Entela Duka <enteladuka@gmail.com>
2018-12-01 14:03:06 -0500

    Added user model with secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181201042103) do
+ActiveRecord::Schema.define(version: 20181201185627) do
 
   create_table "comments", force: :cascade do |t|
     t.string "username"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20181201042103) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
